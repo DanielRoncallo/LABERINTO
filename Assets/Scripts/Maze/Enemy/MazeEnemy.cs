242d2a6b47b88224e42b83ca984768afc33ed39f
DanielRoncallo <roncallodaniel@hotmail.com>
2022-04-29 10:58:25 -0500

    explode prefab

--- Assets/Scripts/Maze/Enemy/MazeEnemy.cs
+++ Assets/Scripts/Maze/Enemy/MazeEnemy.cs
@@ -7,10 +7,7 @@ public class MazeEnemy : MonoBehaviour
     public float speed = 3f;
     private Transform target;
 
-     public float fieldofImpact;
-    public float force;
-
-    public LayerMask LayerToHit;
+   
     // Update is called once per frame
     private void Update()
     {
@@ -19,13 +16,11 @@ public class MazeEnemy : MonoBehaviour
             float step = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, target.position, step);
 
-            explode();
+             
         }
     }
 
-    void explode(){
-
-    }
+   
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
