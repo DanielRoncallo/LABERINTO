7e76b206ac69f73dc9884b7c18ad62fe2bfd8f36
DanielRoncallo <roncallodaniel@hotmail.com>
2022-04-30 00:12:55 -0500

    version 1.0

--- Assets/Scripts/Tools/PlayerController.cs
+++ Assets/Scripts/Tools/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public Text NivelElem;
     static public float TimeCount;
     public Text TimeElem;
-   
+     static private int nivFinal;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +26,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        NivelElem.text = "Nivel: " + nivel.ToString();
-        if (nivel != 5)
+
+        if (nivel != 5 && nivel != 6)
         {
+            nivFinal = nivel;
+            Debug.Log(nivFinal + "nivFinal");
+            NivelElem.text = "Nivel: " + nivel.ToString();
             TimeCount += Time.deltaTime;
+            TimeElem.text = "Tiempo: " + Mathf.Round(TimeCount).ToString();
+        }
+        if (nivel == 5)
+        {
+            NivelElem.text = "Niveles \nsuperados";
+            TimeElem.text = "Tiempo: " + Mathf.Round(TimeCount).ToString();
+        }
+        if (nivel == 6)
+        {
+            Debug.Log(nivFinal + "nivFinal");
+            NivelElem.text = "Nivel: " + nivFinal.ToString();
+            TimeElem.text = "Tiempo: " + Mathf.Round(TimeCount).ToString();
         }
 
-        TimeElem.text = "Tiempo: " + Mathf.Round(TimeCount).ToString();
+
+
     }
     public void NextLevel()
     {
@@ -43,21 +59,26 @@ public class PlayerController : MonoBehaviour
 
     public void SceneLevel()
     {
+        Debug.Log(nivFinal + "nivFinal");
 
         if (nivel == 1)
         {
+            nivFinal = 1;
             SceneManager.LoadScene("Game");
         }
         if (nivel == 2)
         {
+            nivFinal = 2;
             SceneManager.LoadScene("Game2");
         }
         if (nivel == 3)
         {
+            nivFinal = 3;
             SceneManager.LoadScene("Game3");
         }
         if (nivel == 4)
         {
+            nivFinal = 4;
             SceneManager.LoadScene("Game4");
         }
         if (nivel == 5)
